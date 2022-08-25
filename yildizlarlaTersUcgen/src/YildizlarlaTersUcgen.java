bd98e8e3eeb8ea9ef4eddd43b86a423148201c50
SezilAkdogan <111681902+SezilAkdogan@users.noreply.github.com>
2022-08-26 00:37:44 +0300

    Add files via upload

---
+++ yildizlarlaTersUcgen/src/YildizlarlaTersUcgen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class YildizlarlaTersUcgen {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print(" Ters Üçgenin Basamak sayısını girin : ");
+        int n = input.nextInt();
+
+        for( int i = 1; i < n; i++){
+            for( int k=1; k <= i; k++){
+                System.out.print(" ");
+            }
+            for( int j = 1; j <= 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
