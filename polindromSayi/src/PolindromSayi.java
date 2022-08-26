3d2d836832574a53b37e5c673a770c1a294bf8b1
SezilAkdogan <111681902+SezilAkdogan@users.noreply.github.com>
2022-08-26 16:00:24 +0300

    Add files via upload

---
+++ polindromSayi/src/PolindromSayi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class PolindromSayi {
+    static boolean isPolindrom(int number){
+        int temp = number, reverseNumber = 0, lastNumber;
+        while(temp != 0){
+            lastNumber = temp % 10;
+
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+
+            temp /= 10;
+        }
+        if(number == reverseNumber) {
+            return true;
+
+        }else {
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        number = input.nextInt();
+
+        System.out.println(isPolindrom(number));
+    }
+}
